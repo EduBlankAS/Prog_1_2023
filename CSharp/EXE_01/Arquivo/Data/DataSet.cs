e844d94a2a57e3634246e386ca8f50f599da4fcf
Eduardo Blank Arrais Silva <101676501+EduBlankAS@users.noreply.github.com>
2023-06-05 22:06:39 -0300

    Add files via upload

---
+++ CSharp/EXE_01/Arquivo/Data/DataSet.cs
@@ -0,0 +1,9 @@
+using Arquivo.Models;
+namespace Arquivo.Data
+{
+    public static class DataSet
+    {
+        public static List<Client> Clients
+            = new List<Client>();
+    }
+}
---
+++ CSharp/EXE_01/Arquivo/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace Arquivo.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? CPF { get; set; }
+        public string? Email { get; set; }
+    }
+}
