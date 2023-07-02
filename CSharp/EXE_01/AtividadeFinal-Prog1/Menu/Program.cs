9f46b87cb9e4578d1a05fe8582d5c77e1b1cc59e
Eduardo Blank Arrais Silva <101676501+EduBlankAS@users.noreply.github.com>
2023-07-01 22:44:18 -0300

    Add files via upload

---
+++ CSharp/EXE_01/AtividadeFinal-Prog1/Menu/Program.cs
@@ -0,0 +1,54 @@
+﻿using Arquivos.Views;
+using Menu.Utils;
+using manipulacao_de_datas;
+
+/*
+    Programa para leitura de dados de pessoas
+    e exportação em arquivo .txt
+*/
+Bootstrapper.ChargeClients();
+
+int option = 0;
+
+do
+{
+    DateTime data = DateTime.Now;
+    Console.WriteLine("-------------------");
+    Console.WriteLine(data.ToLocalTime());
+    Console.WriteLine("--------Menu-------");
+    Console.WriteLine("1 - Clientes");
+    Console.WriteLine("2 - Animais");
+    Console.WriteLine("3 - Médicos");
+    Console.WriteLine("4 - Clínicas");
+    Console.WriteLine("0 - SAIR");
+    
+    option = Convert.ToInt32(Console.ReadLine());
+
+    switch(option)
+    {
+        case 1 :
+            Console.WriteLine("Opção Clientes");
+            ClientView clientView = new ClientView();
+        break;
+
+        case 2 :
+            Console.WriteLine("Opção Animais");
+            AnimalsView animalsViews = new AnimalsView();
+        break;
+        
+        case 3 :
+            Console.WriteLine("Opção Médicos");
+            MedicosView medicoViews = new MedicosView();
+        break;
+        
+        case 4 :
+            Console.WriteLine("Opção Clínicas");
+            ClinicasViews clinicaViews = new ClinicasViews();
+        break;
+    }
+
+
+
+
+}
+while(option > 0);
